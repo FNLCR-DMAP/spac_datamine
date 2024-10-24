956f8a1b27c14b4b2ad4b7955bb78eb9b0c442c3
oeldaghar <oeldagha@purdue.edu>
2024-10-24 16:54:42 -0400

    added missing_label as parameter for knn clustering and updated unittests/docstring

--- tests/test_transformations/test_knn_clustering.py
+++ tests/test_transformations/test_knn_clustering.py
@@ -36,12 +36,17 @@ class TestKnnClustering(unittest.TestCase):
         data_rows = np.vstack((mean_10, mean_100))
 
         # Generate class labels, label 0 = mean 10, label 1 = mean 100
-        class_labels = np.array([0] * (n_rows // 2) + [1] * (n_rows // 2), dtype=object)
-
+        full_class_labels = np.array([0] * (n_rows // 2) + [1] * (n_rows // 2), dtype=object)
+        class_labels = np.array([x for x in full_class_labels],dtype=object)
+ 
         # Replace ~50% of class labels with "missing" values
-        mask = np.random.rand(*class_labels.shape) < 0.5
+        mask = np.random.rand(*full_class_labels.shape) < 0.5
         class_labels[mask] = "no_label"
 
+        # annotation to test "missing_label" parameter
+        alt_class_labels = np.array([x for x in class_labels],dtype=object)
+        alt_class_labels[alt_class_labels == "no_label"] = np.nan
+        
         # Combine data columns with class labels
         self.syn_dataset = data_rows
 
@@ -55,88 +60,65 @@ class TestKnnClustering(unittest.TestCase):
 
         self.syn_data.obs["classes"] = class_labels
 
+        # annotation with all labels missing
+        self.syn_data.obs["all_missing_classes"] = np.array(["no_label" for x in full_class_labels])
+        # annotation with all labels present
+        self.syn_data.obs["no_missing_classes"] = full_class_labels
+        self.syn_data.obs["alt_classes"] = alt_class_labels
+
         # The string for column where class labels stored in obs
         self.annotation = "classes"
+        self.alt_annotation = "alt_classes"
         # The layer used for knn
         self.layer = "counts"
         # The features used for syn_data
         self.syn_features = ["gene1", "gene2"]
 
-        ################
-        # adata (iris) #
-        ################
-        # Using sklearn iris dataset
-        n_iris = 150
-        iris_df = load_iris(as_frame=True)
-
-        # Set up AnnData object with 100 rows, all features
-        # and each row's class in obs
-        self.adata = AnnData(
-            X=iris_df.data.iloc[:n_iris, :],
-            var=pd.DataFrame(index=iris_df.data.columns),
-        )
-
-        # Replace ~50% of class labels with "missing" values
-        self.adata.obs["classes"] = iris_df.target.iloc[:n_iris].to_numpy()
-        iris_mask = np.random.rand(*self.adata.obs["classes"].shape) < 0.5
-        self.adata.obs["classes"][iris_mask] = "no_label"
-
-        # set all labels to missing
-        self.adata.obs["all_missing_classes"] = iris_df.target.iloc[:n_iris].to_numpy()
-        self.adata.obs["all_missing_classes"][:] = "no_label"
-
-        # select all data labels, none missing
-        self.adata.obs["no_missing_classes"] = iris_df.target.iloc[:n_iris].to_numpy()
-
-        self.adata.layers["counts"] = iris_df.data.iloc[:n_iris, :]
-
-        # The features within the iris dataset
-        self.features = iris_df.data.columns.to_list()
 
     def test_typical_case(self):
         # This test checks if the function correctly adds 'knn' to the
         # AnnData object's obs attribute and if it correctly sets
         # 'knn_features' in the AnnData object's uns attribute.
         knn_clustering(
-            adata=self.adata,
-            features=self.features,
+            adata=self.syn_data,
+            features=self.syn_features,
             annotation=self.annotation,
             layer=self.layer,
         )
-        self.assertIn("knn", self.adata.obs)
-        self.assertEqual(self.adata.uns["knn_features"], self.features)
+        self.assertIn("knn", self.syn_data.obs)
+        self.assertEqual(self.syn_data.uns["knn_features"], self.syn_features)
 
     def test_output_annotation(self):
         # This test checks if the function correctly adds the "output_layer"
         # to the # AnnData object's obs attribute
         output_annotation_name = "my_output_annotation"
         knn_clustering(
-            adata=self.adata,
-            features=self.features,
+            adata=self.syn_data,
+            features=self.syn_features,
             annotation=self.annotation,
             layer=self.layer,
             output_annotation=output_annotation_name,
         )
-        self.assertIn(output_annotation_name, self.adata.obs)
+        self.assertIn(output_annotation_name, self.syn_data.obs)
 
     def test_layer_none_case(self):
         # This test checks if the function works correctly when layer is None.
         knn_clustering(
-            adata=self.adata,
-            features=self.features,
+            adata=self.syn_data,
+            features=self.syn_features,
             annotation=self.annotation,
             layer=None,
         )
-        self.assertIn("knn", self.adata.obs)
-        self.assertEqual(self.adata.uns["knn_features"], self.features)
+        self.assertIn("knn", self.syn_data.obs)
+        self.assertEqual(self.syn_data.uns["knn_features"], self.syn_features)
 
     def test_invalid_k(self):
         # This test checks if the function raises a ValueError when the
         # k argument is not a positive integer.
         with self.assertRaises(ValueError):
             knn_clustering(
-                adata=self.adata,
-                features=self.features,
+                adata=self.syn_data,
+                features=self.syn_features,
                 annotation=self.annotation,
                 layer=self.layer,
                 k="invalid",
@@ -148,8 +130,8 @@ class TestKnnClustering(unittest.TestCase):
         # all datapoints labeled
         with self.assertRaises(ValueError):
             knn_clustering(
-                adata=self.adata,
-                features=self.features,
+                adata=self.syn_data,
+                features=self.syn_features,
                 annotation="no_missing_classes",
                 layer=self.layer,
             )
@@ -157,8 +139,8 @@ class TestKnnClustering(unittest.TestCase):
         # no datapoints labeled
         with self.assertRaises(ValueError):
             knn_clustering(
-                adata=self.adata,
-                features=self.features,
+                adata=self.syn_data,
+                features=self.syn_features,
                 annotation="all_missing_classes",
                 layer=self.layer,
             )
@@ -191,6 +173,33 @@ class TestKnnClustering(unittest.TestCase):
 
         self.assertEqual(len(np.unique(self.syn_data.obs[output_annotation])), 2)
 
+    def test_missing_label(self):
+        # This test checks that the missing label parameter works as intended
+        #first knn call with normal data
+        knn_clustering(
+            adata=self.syn_data,
+            features=self.syn_features,
+            annotation=self.annotation,
+            layer="counts",
+            k=50,
+            output_annotation="knn_1",
+            associated_table=None,
+            missing_label = "no_label"
+        )
+        #second knn call with alt_class data
+        knn_clustering(
+            adata=self.syn_data,
+            features=self.syn_features,
+            annotation=self.alt_annotation,
+            layer="counts",
+            k=50,
+            output_annotation="knn_2",
+            associated_table=None,
+            missing_label = np.nan
+        )
+
+        #assert that they produce the same final label
+        self.assertTrue(all(self.syn_data.obs["knn_1"]==self.syn_data.obs["knn_2"]))
 
 if __name__ == "__main__":
     unittest.main()
